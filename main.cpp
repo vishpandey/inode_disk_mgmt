957e3cda19488012ebbdbca5388b02ccb3f213fe
vishpandey <vishpandey2012@gmail.com>
2021-11-16 12:29:16 +0530

    intial run for create disk

--- main.cpp
+++ main.cpp
@@ -1,49 +1,50 @@
 #include <iostream>
 #include <unistd.h>
+#include <cmath>
 
 using namespace std;
 
 #define DATA_SIZE 512 * 1024 * 1024
 #define BLOCK_SIZE 4096
-#define NO_OF_DATA_BLOCKS (DATA_SIZE / BLOCK_SIZE)
-#define NO_OF_INODES (0.6 * NO_OF_DATA_BLOCKS)
-#define NO_OF_SB_BLOCKS (NO_OF_DATA_BLOCKS + NO_OF_INODES) / BLOCK_SIZE
-#define NO_OF_TOTAL_BLOCKS (NO_OF_DATA_BLOCKS + NO_OF_INODES + NO_OF_SB_BLOCKS)
+#define NO_OF_DATA_BLOCKS 131072
+#define NO_OF_INODES 78644
+#define NO_OF_SB_BLOCKS 52
+#define NO_OF_TOTAL_BLOCKS 209767
 
-string diskPrefix = "disks/d_"
+string diskPrefix = "disks/d_";
 string diskpath;
 
 
 struct SuperBlock {
 	bool free_inodes[NO_OF_INODES];
 	bool free_data_blocks[NO_OF_DATA_BLOCKS];  
-}
+};
 
 struct fileInodeMap {
 	char filename[10];
 	int inodeIndex;
-}
+};
 
 struct Inode {
 	char filename[10];
 	char filepath[100];
 
 	int bPointer[10];
-}
+};
 
 long long int total_disk_size = 1 * sizeof(struct SuperBlock) + 
 								NO_OF_INODES * sizeof(struct Inode) + 
-								NO_OF_DATA_BLOCKS * DATA_BLOCK_SIZE;
+								NO_OF_DATA_BLOCKS * BLOCK_SIZE;
 
 struct fileInodeMap globalFileInodeMap[NO_OF_INODES];
 struct SuperBlock globalSuperBlock;
 
 void initializeDisk(struct SuperBlock *sb, FILE *diskDescriptor) {
 	for(int i = 0; i < NO_OF_INODES; i++) {
-		sb.free_inodes[i] = true;
+		sb->free_inodes[i] = true;
 	}
 	for(int i = 0; i < NO_OF_DATA_BLOCKS; i++) {
-		sb.free_data_blocks[i] = true;
+		sb->free_data_blocks[i] = true;
 	}
 
 	int sb_size = ceil(((float)sizeof(sb)));
@@ -66,7 +67,7 @@ void initializeDisk(struct SuperBlock *sb, FILE *diskDescriptor) {
 	int offset = sb_size + NO_OF_INODES * fileInodeMapSize;
 	fseek(diskDescriptor, offset, SEEK_SET);
 
-	struct inode tempInodeArr[NO_OF_INODES];
+	struct Inode tempInodeArr[NO_OF_INODES];
 	fwrite(&tempInodeArr, fileInodeSize, NO_OF_INODES, diskDescriptor);
 
 	cout << "written " << NO_OF_INODES << " inodes" << endl;
@@ -76,10 +77,10 @@ void initializeDisk(struct SuperBlock *sb, FILE *diskDescriptor) {
 
 
 void create_disk(string diskname, string diskpath) {
-	FILE *diskDescriptor = fopen(diskpath, "wb"); 
+	FILE *diskDescriptor = fopen(diskpath.c_str(), "wb"); 
 
 	fseek(diskDescriptor, total_disk_size, SEEK_SET);
-	fputc('\0', fp);
+	fputc('\0', diskDescriptor);
 
 	struct SuperBlock *sb = (struct SuperBlock*)malloc(1 * sizeof(struct SuperBlock));;
 	initializeDisk(sb, diskDescriptor);
@@ -97,12 +98,12 @@ int main() {
 		string diskname;
 
 		if(option == 1) {
-			cout >> "Enter disk name : ";
+			cout << "Enter disk name : ";
 			cin >>  diskname;
 
 			diskpath = diskPrefix + diskname;
 			
-			if(access(diskpath, F_OK) != -1) {
+			if(access(diskpath.c_str(), F_OK) != -1) {
 				cout << "Disk already exists" << endl;
 				continue;
 			}
